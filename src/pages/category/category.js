0d8e679d9cd588563f90196ebb4c533b534c3a28
JRG-zhangshijun <462895506@qq.com>
2018-01-17 21:27:42 +0800

    列表页完成 问题:touchmove不能触发

--- src/pages/category/category.js
+++ src/pages/category/category.js
@@ -4,7 +4,8 @@ import './category.css'
 import Vue from 'vue'
 import axios from 'axios'
 import url from 'js/api.js'
-import Foot from 'components/Foot.vue'
+// import Foot from 'components/Foot.vue'
+import mixin from 'js/mixin.js'
 
 new Vue({
   el: '#app',
@@ -39,14 +40,13 @@ new Vue({
       axios.post(url.rank).then(res => {
         this.rankData = res.data.data
       })
+    },
+    toSearch(list){
+      location.href = `search.html?keyword=${list.name}&id=${list.id}`
     }
   },
-  components: {
-    Foot
-  },
-  filters: {
-    number(price){
-      return price + '.00'
-    }
-  }
+  // components: {
+  //   Foot
+  // }
+  mixins: [mixin]
 })
